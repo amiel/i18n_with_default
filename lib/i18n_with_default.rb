4760f2586127d0087f6eaf471afe51a9e076435c
Amiel Martin <amiel.martin@gmail.com>
2009-10-12 09:45:06 -0700

    initial import

---
+++ lib/i18n_with_default.rb
@@ -0,0 +1,18 @@
+module I18n
+	unless self.respond_to? :translate_with_en_default
+		class << self
+			def translate_with_en_default(key, options = {})
+				return translate_without_en_default(key, options) if locale.to_sym == :en
+				translate_without_en_default key, options.merge(:raise => true)
+			rescue MissingTranslationData => e
+				begin
+					translate_without_en_default key, options.merge(:raise => true, :locale => :en)
+				rescue MissingTranslationData
+					e.message
+				end
+			end
+			alias_method_chain :translate, :en_default
+			alias_method :t, :translate
+		end
+	end
+end
